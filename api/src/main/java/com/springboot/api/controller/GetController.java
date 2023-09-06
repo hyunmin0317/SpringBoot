4b7bee2301af9e44f0e889751d72797fa7895b8c
hyunmin0317 <choihm9903@naver.com>
2023-09-06 21:00:43 +0900

    feat: GET 메서드 (#4)

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
         return "Hello World";
