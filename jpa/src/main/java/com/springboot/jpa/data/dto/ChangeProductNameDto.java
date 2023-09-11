3f15ac7d335d2f63e86983e4cfa30d89d4ffa200
hyunmin0317 <choihm9903@naver.com>
2023-09-11 13:38:35 +0900

    refactor: ChangeProductNameDto (#6)

--- jpa/src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
+++ jpa/src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
@@ -1,34 +1,16 @@
 package com.springboot.jpa.data.dto;
 
+import lombok.*;
 
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangeProductNameDto {
 
     private Long number;
 
     private String name;
-
-    public ChangeProductNameDto(Long number, String name) {
-        this.number = number;
-        this.name = name;
-    }
-
-    public ChangeProductNameDto() {
-    }
-
-    public Long getNumber() {
-        return this.number;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
