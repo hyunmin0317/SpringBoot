0de58c8736923df82a3e199f79a73e28e6e2a8c6
hyunmin0317 <choihm9903@naver.com>
2023-09-13 21:05:47 +0900

    feat: 다대다 양방향 매핑 (#9)

--- relationship/src/test/java/com/springboot/relationship/data/repository/ProducerRepositoryTest.java
+++ relationship/src/test/java/com/springboot/relationship/data/repository/ProducerRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-// 예제 9.22
 @SpringBootTest
 class ProducerRepositoryTest {
 
@@ -37,6 +36,35 @@ class ProducerRepositoryTest {
         producerRepository.saveAll(Lists.newArrayList(producer1, producer2));
 
         System.out.println(producerRepository.findById(1L).get().getProducts());
+    }
+
+    @Test
+    @Transactional
+    void relationshipTest2() {
+        Product product1 = saveProduct("동글펜", 500, 1000);
+        Product product2 = saveProduct("네모 공책", 100, 2000);
+        Product product3 = saveProduct("지우개", 152, 1234);
+
+        Producer producer1 = saveProducer("flature");
+        Producer producer2 = saveProducer("wikibooks");
+
+        producer1.addProduct(product1);
+        producer1.addProduct(product2);
+        producer2.addProduct(product2);
+        producer2.addProduct(product3);
+
+        product1.addProducer(producer1);
+        product2.addProducer(producer1);
+        product2.addProducer(producer2);
+        product3.addProducer(producer2);
+
+        producerRepository.saveAll(Lists.newArrayList(producer1, producer2));
+        productRepository.saveAll(Lists.newArrayList(product1, product2, product3));
+
+        System.out.println("products : " + producerRepository.findById(1L).get().getProducts());
+
+        System.out.println("producers : " + productRepository.findById(2L).get().getProducers());
+
     }
 
     private Product saveProduct(String name, Integer price, Integer stock) {
