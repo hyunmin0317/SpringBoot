6310ba49d63dc18753d87bf97e9c1cde2125b576
hyunmin0317 <choihm9903@naver.com>
2023-09-14 22:21:48 +0900

    feat: 로그인, 회원가입 Controller 작성 (#13)

--- security/src/main/java/com/springboot/security/data/entity/Product.java
+++ security/src/main/java/com/springboot/security/data/entity/Product.java
@@ -3,29 +3,35 @@ package com.springboot.security.data.entity;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString(exclude = "name")
-@Table(name = "product")
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@Table
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long number;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
@@ -34,7 +40,10 @@ public class Product {
     @Column(nullable = false)
     private Integer stock;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+
 }
