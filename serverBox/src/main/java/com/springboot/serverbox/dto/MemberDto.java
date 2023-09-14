8eb8adfbed1656a2fd0cd631f8d3886d5ec4815b
hyunmin0317 <choihm9903@naver.com>
2023-09-14 20:48:51 +0900

    feat: ServerBox 프로젝트 완성 (#12)

---
+++ serverBox/src/main/java/com/springboot/serverbox/dto/MemberDto.java
@@ -0,0 +1,11 @@
+package com.springboot.serverbox.dto;
+
+import lombok.Data;
+
+
+@Data
+public class MemberDto {
+    private String name;
+    private String email;
+    private String organization;
+}
