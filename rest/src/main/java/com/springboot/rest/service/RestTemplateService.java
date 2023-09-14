c22f19c21649f88de6bc3204d42982343bcc856b
hyunmin0317 <choihm9903@naver.com>
2023-09-14 21:12:13 +0900

    feat: RestTemplate 커스텀 설정 (#12)

--- rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
+++ rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -1,12 +1,17 @@
 package com.springboot.rest.service;
 
 import com.springboot.rest.dto.MemberDto;
+import java.net.URI;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-import java.net.URI;
 
 
 @Service
@@ -93,5 +98,27 @@ public class RestTemplateService {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<MemberDto> responseEntity = restTemplate.exchange(requestEntity, MemberDto.class);
         return responseEntity;
+    }
+
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        HttpClient client = HttpClientBuilder.create()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate;
     }
 }
