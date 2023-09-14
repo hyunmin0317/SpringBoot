f5e7f26cce590f08358df715828a3b314c56b0ed
hyunmin0317 <choihm9903@naver.com>
2023-09-14 20:57:49 +0900

    feat: GET 형식의 RestTemplate 작성 (#12)

---
+++ rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -0,0 +1,54 @@
+package com.springboot.rest.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+
+@Service
+public class RestTemplateService {
+
+    public String getName() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithPathVariable() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/{name}")
+                .encode()
+                .build()
+                .expand("Flature")
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithParameter() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/param")
+                .queryParam("name", "Flature")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+        return responseEntity.getBody();
+    }
+}
